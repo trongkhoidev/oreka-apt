8c3163b7bf91c4158224f047e29784baad2e30d0
trongkhoi <yugodevbc@gmail.com>
2025-06-29 00:35:30 +0700

    init commit

--- evm-frontend/src/utils/market.ts
+++
@@ -1,7 +0,0 @@
-export const determineMarketResult = (
-    finalPrice: number,
-    strikePrice: number
-  ): 'LONG' | 'SHORT' => {
-    return finalPrice < strikePrice ? 'SHORT' : 'LONG';
-  };
-  
