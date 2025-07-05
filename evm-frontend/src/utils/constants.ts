122615e3c7a329919bbe60fd7c65e0a071db6537
trongkhoi <yugodevbc@gmail.com>
2025-07-05 13:00:37 +0700

    feat(frontend): update list markets

--- evm-frontend/src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-// Define constants for the strike price
-import { ethers } from 'ethers';
-
-// Use BigNumber to avoid precision issues with large numbers
-export const STRIKE_PRICE_DECIMALS = 8;
-export const STRIKE_PRICE_MULTIPLIER = ethers.BigNumber.from(10).pow(STRIKE_PRICE_DECIMALS);
